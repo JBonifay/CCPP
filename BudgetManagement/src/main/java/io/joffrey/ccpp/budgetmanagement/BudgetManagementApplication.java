6715d6c19e4e54539f59c657385d263d8f02ac8e
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-09 00:00:00 +0100

    add modules

---
+++ BudgetManagement/src/main/java/io/joffrey/ccpp/budgetmanagement/BudgetManagementApplication.java
@@ -0,0 +1,13 @@
+package io.joffrey.ccpp.budgetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetManagementApplication.class, args);
+    }
+
+}

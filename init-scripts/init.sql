aad9e8681cae1f273cac595d7767ee6635b27118
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-22 00:00:00 +0100

    add kafka ui

---
+++ init-scripts/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE "user-management";
+CREATE DATABASE "ideation";
+CREATE DATABASE "notification";
+CREATE DATABASE "project-planning";
+CREATE DATABASE "user-management";
+CREATE DATABASE "workspace";

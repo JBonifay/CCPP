f6ecaece3cae3455a06231b37bca1bd9691ffd28
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-22 00:00:00 +0100

    fix sommandBus

--- user-management/src/main/java/fr/joffreybonifay/ccpp/usermanagement/infrastructure/configuration/BeanConfig.java
+++ user-management/src/main/java/fr/joffreybonifay/ccpp/usermanagement/infrastructure/configuration/BeanConfig.java
@@ -5,6 +5,9 @@ import fr.joffreybonifay.ccpp.shared.command.SimpleCommandBus;
 import fr.joffreybonifay.ccpp.shared.eventstore.EventStore;
 import fr.joffreybonifay.ccpp.usermanagement.application.command.AssignUserToWorkspaceCommand;
 import fr.joffreybonifay.ccpp.usermanagement.application.command.AssignUserToWorkspaceCommandHandler;
+import fr.joffreybonifay.ccpp.usermanagement.application.command.RegisterNewUserCommand;
+import fr.joffreybonifay.ccpp.usermanagement.application.command.RegisterNewUserCommandHandler;
+import fr.joffreybonifay.ccpp.usermanagement.domain.service.UserUniquenessChecker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,10 +22,14 @@ public class BeanConfig {
     }
 
     @Bean
-    CommandBus commandBus(EventStore eventStore) {
+    CommandBus commandBus(
+            EventStore eventStore,
+            PasswordEncoder passwordEncoder,
+            UserUniquenessChecker userUniquenessChecker
+    ) {
         SimpleCommandBus simpleCommandBus = new SimpleCommandBus();
+        simpleCommandBus.register(RegisterNewUserCommand.class, new RegisterNewUserCommandHandler(eventStore, passwordEncoder, userUniquenessChecker));
         simpleCommandBus.register(AssignUserToWorkspaceCommand.class,new AssignUserToWorkspaceCommandHandler(eventStore));
-        simpleCommandBus.register(AssignUserToWorkspaceCommand.class, new AssignUserToWorkspaceCommandHandler(eventStore));
         return simpleCommandBus;
     }
 

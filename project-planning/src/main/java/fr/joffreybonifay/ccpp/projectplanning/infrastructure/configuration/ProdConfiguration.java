babddf5cd9921646d243db60ad2709195e501267
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-24 00:00:00 +0100

    fix outbox

--- project-planning/src/main/java/fr/joffreybonifay/ccpp/projectplanning/infrastructure/configuration/ProdConfiguration.java
+++ project-planning/src/main/java/fr/joffreybonifay/ccpp/projectplanning/infrastructure/configuration/ProdConfiguration.java
@@ -9,7 +9,6 @@ import fr.joffreybonifay.ccpp.shared.eventstore.JpaEventStore;
 import fr.joffreybonifay.ccpp.shared.indempotency.ProcessedEventRepository;
 import fr.joffreybonifay.ccpp.shared.outbox.OutboxProcessor;
 import fr.joffreybonifay.ccpp.shared.outbox.OutboxRepository;
-import fr.joffreybonifay.ccpp.shared.outbox.OutboxWorker;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,18 +45,11 @@ public class ProdConfiguration {
 
     @Bean
     OutboxProcessor outboxProcessor(
-            OutboxWorker outboxWorker,
-            OutboxRepository outboxRepository
-    ) {
-        return new OutboxProcessor(outboxWorker, outboxRepository);
-    }
-
-    @Bean
-    OutboxWorker outboxWorker(
             KafkaTemplate<String, String> kafkaTemplate,
             ObjectMapper objectMapper,
             OutboxRepository outboxRepository
     ) {
-        return new OutboxWorker(outboxRepository, kafkaTemplate, objectMapper, "project-planning-events");
+        return new OutboxProcessor(outboxRepository, kafkaTemplate, objectMapper, "project-planning-events");
     }
+
 }
--- workspace/src/main/java/fr/joffreybonifay/ccpp/workspace/infrastructure/configuration/ProdConfiguration.java
+++ workspace/src/main/java/fr/joffreybonifay/ccpp/workspace/infrastructure/configuration/ProdConfiguration.java
@@ -8,7 +8,6 @@ import fr.joffreybonifay.ccpp.shared.eventstore.JpaEventStore;
 import fr.joffreybonifay.ccpp.shared.indempotency.ProcessedEventRepository;
 import fr.joffreybonifay.ccpp.shared.outbox.OutboxProcessor;
 import fr.joffreybonifay.ccpp.shared.outbox.OutboxRepository;
-import fr.joffreybonifay.ccpp.shared.outbox.OutboxWorker;
 import fr.joffreybonifay.ccpp.workspace.infrastructure.messaging.ProjectPlanningEventListener;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.Bean;
@@ -46,18 +45,11 @@ public class ProdConfiguration {
 
     @Bean
     OutboxProcessor outboxProcessor(
-            OutboxWorker outboxWorker,
-            OutboxRepository outboxRepository
-    ) {
-        return new OutboxProcessor(outboxWorker, outboxRepository);
-    }
-
-    @Bean
-    OutboxWorker outboxWorker(
             KafkaTemplate<String, String> kafkaTemplate,
             ObjectMapper objectMapper,
             OutboxRepository outboxRepository
     ) {
-        return new OutboxWorker(outboxRepository, kafkaTemplate, objectMapper, "workspace-events");
+        return new OutboxProcessor(outboxRepository, kafkaTemplate, objectMapper, "workspace-events");
     }
+
 }

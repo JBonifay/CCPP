b2a7de8fae369daa327428ad9d4adfe17e556c73
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-26 00:00:00 +0100

    fix issue

---
+++ user-management/src/test/java/fr/joffreybonifay/ccpp/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,11 @@
+package fr.joffreybonifay.ccpp.usermanagement;
+
+import org.junit.jupiter.api.Test;
+
+class UserManagementApplicationTests extends AbstractE2eTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

e743c62eb1e86afdecd91c8b1931956d319409f3
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-26 00:00:00 +0100

    refactor code

--- project-planning/src/main/java/fr/joffreybonifay/ccpp/projectplanning/infrastructure/spi/UuidParticipantIdGenerator.java
+++ project-planning/src/main/java/fr/joffreybonifay/ccpp/projectplanning/infrastructure/spi/UuidParticipantIdGenerator.java
@@ -2,9 +2,11 @@ package fr.joffreybonifay.ccpp.projectplanning.infrastructure.spi;
 
 import fr.joffreybonifay.ccpp.projectplanning.domain.spi.ParticipantIdGenerator;
 import fr.joffreybonifay.ccpp.projectplanning.domain.valueobject.ParticipantId;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class UuidParticipantIdGenerator implements ParticipantIdGenerator {
 
     @Override
--- project-planning/src/main/java/fr/joffreybonifay/ccpp/projectplanning/infrastructure/spi/UuidProjectIdGenerator.java
+++ project-planning/src/main/java/fr/joffreybonifay/ccpp/projectplanning/infrastructure/spi/UuidProjectIdGenerator.java
@@ -2,9 +2,11 @@ package fr.joffreybonifay.ccpp.projectplanning.infrastructure.spi;
 
 import fr.joffreybonifay.ccpp.shared.domain.identities.ProjectId;
 import fr.joffreybonifay.ccpp.projectplanning.domain.spi.ProjectIdGenerator;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class UuidProjectIdGenerator implements ProjectIdGenerator {
 
     @Override

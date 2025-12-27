a48c7af6e1ab424f6e5789c1918b10d5090f39b8
Joffrey B. <joffreybonifay83@gmail.com>
2025-12-28 00:00:00 +0100

    change dashboard

--- frontend/src/app/layouts/app-layout/app-layout.component.ts
+++ frontend/src/app/layouts/app-layout/app-layout.component.ts
@@ -1,57 +1,18 @@
-import {Component, inject} from '@angular/core';
-import {AuthStore} from '../../core';
-import {MenuItem, PrimeIcons} from 'primeng/api';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {AppRoutePaths, RouteTokens} from '../../app.routes';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {TopbarComponent} from './topbar.component';
+import {SidebarComponent} from './sidebar.component';
 
 @Component({
   selector: 'app-layout',
   imports: [
     RouterOutlet,
-    RouterLinkActive,
-    RouterLink
+    TopbarComponent,
+    SidebarComponent
   ],
   templateUrl: './app-layout.component.html',
   styleUrls: ['./app-layout.component.css'],
 })
 export class AppLayoutComponent {
 
-  readonly authStore = inject(AuthStore);
-
-  items: MenuItem[] = [
-    {
-      label: 'Navigation',
-      items: [
-        {
-          icon: PrimeIcons.HOME,
-          routerLink: AppRoutePaths.projects(),
-          exact: true
-        },
-        {
-          icon: PrimeIcons.FOLDER,
-          routerLink: AppRoutePaths.project(1)
-        }
-      ]
-    },
-    {
-      label: 'Management',
-      items: [
-        {
-          icon: PrimeIcons.USERS,
-          routerLink: ['/customers']
-        },
-        {
-          icon: 'pi pi-chart-bar',
-          routerLink: ['/reports']
-        }
-      ]
-    },
-  ];
-
-  protected logout() {
-    this.authStore.logout();
-  }
-
-  protected readonly PrimeIcons = PrimeIcons;
-  protected readonly RouteTokens = RouteTokens;
 }
---
+++ frontend/src/app/layouts/app-layout/sidebar.component.ts
@@ -0,0 +1,55 @@
+import {AppRoutePaths, RouteTokens} from '../../app.routes';
+import {MenuItem, PrimeIcons} from 'primeng/api';
+import {AuthStore} from '../../core';
+import {Component, inject} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+})
+export class SidebarComponent {
+
+  readonly authStore = inject(AuthStore);
+
+  items: MenuItem[] = [
+    {
+      label: 'Navigation',
+      items: [
+        {
+          icon: PrimeIcons.HOME,
+          routerLink: AppRoutePaths.projects(),
+          exact: true
+        },
+        {
+          icon: PrimeIcons.FOLDER,
+          routerLink: AppRoutePaths.project(1)
+        }
+      ]
+    },
+    {
+      label: 'Management',
+      items: [
+        {
+          icon: PrimeIcons.USERS,
+          routerLink: ['/customers']
+        },
+        {
+          icon: 'pi pi-chart-bar',
+          routerLink: ['/reports']
+        }
+      ]
+    },
+  ];
+
+  protected logout() {
+    this.authStore.logout();
+  }
+
+  protected readonly RouteTokens = RouteTokens;
+  protected readonly PrimeIcons = PrimeIcons;
+}
